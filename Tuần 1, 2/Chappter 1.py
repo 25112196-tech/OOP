2114a394526712259175ebed9df9a7b62555cb60
Anh Thu <ta6772800@gmai.com>
2026-03-20 09:08:57 +0700

    Upload bai tap OOP sach se

---
+++ Tuần 1, 2/Chappter 1.py
@@ -0,0 +1,29 @@
+<<<<<<< HEAD:Tuần 1, 2/Chappter 1.py
+minutes = 42
+seconds = minutes * 60 + 42
+print (seconds, "seconds")
+
+km = 10
+miles = km / 1.61
+print (round(miles,2), "miles")
+
+averagepace = seconds / miles
+print (round (averagepace, 2), "miles per seconds")
+print (round (averagepace/60, 2), "miles per minutes")
+averagespeed = miles / (seconds /3600)
+print (round (averagespeed, 2), "miles per hour")
+
+=======
+minutes = 42
+seconds = minutes * 60 + 42
+print (seconds, "seconds")
+km = 10
+miles = km / 1.61
+print (round(miles,2), "miles")
+averagepace = seconds / miles
+print (round (averagepace, 2), "miles per seconds")
+print (round (averagepace/60, 2), "miles per minutes")
+averagespeed = miles / (seconds /3600)
+print (round (averagespeed, 2), "miles per hour")
+
+>>>>>>> fa82dd1c86c4e7ad90f250cefb55fbf62c51214c:Chappter 1.py
